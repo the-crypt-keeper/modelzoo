cf6d8501b410d48ff4ad4f22da63215e4568aa99
Mike <kryptk@gmail.com>
2024-12-27 17:08:27 +0000

    refactor to use litellm-proxy wip

--- kobold_handler.py
+++ kobold_handler.py
@@ -5,16 +5,37 @@ from typing import Any, Optional, Union, Protocol
 from litellm import CustomLLM
 from litellm.types.utils import ImageResponse, ImageObject
 
-class AsyncHTTPHandler(Protocol):
-    async def post(self, url: str, json: dict) -> Any: ...
-
 class KoboldCpp(CustomLLM):
-    async def aimage_generation(self, model: str, prompt: str, model_response: ImageResponse, optional_params: dict, logging_obj: Any, timeout: Optional[Union[float, httpx.Timeout]] = None, client: Optional[AsyncHTTPHandler] = None,) -> ImageResponse:
+    async def aimage_generation(self, model: str, prompt: str, model_response: ImageResponse, optional_params: dict, logging_obj: Any, timeout: Optional[Union[float, httpx.Timeout]] = None, client = None, **kwargs) -> ImageResponse:
         if client is None:
-            raise ValueError("HTTP client is required")
+            client = httpx.AsyncClient(base_url=kwargs['api_base'], timeout=timeout)
+        
+        # Health check. We don't actually need to generate anything, just check the server is alive.
+        if prompt == "test from litellm":
+            response = await client.get("/api/v1/info/version")
+            response.raise_for_status()          
+            return ImageResponse()
+
+        if optional_params.get('n', 1) != 1: 
+            raise ValueError('n parameter is not supported by the proxy')
+        if 'size' in optional_params:
+            optional_params['width'], optional_params['height'] = optional_params.pop('size').split('x')        
+        if 'steps' not in optional_params:
+            optional_params['steps'] = 8            
+        optional_params['prompt'] = prompt
+        
+
+        print("aimage_generation() called:", optional_params, "timeout=", timeout)
+        try:
+            response = await client.post("/sdapi/v1/txt2img", json=optional_params)
+        except Exception as e:
+            print("HTTP ERROR:", e)
             
-        response = await client.post("/sdapi/v1/txt2img", json={"prompt": prompt})
-        result = response.json()
+        try:
+            result = response.json()
+        except Exception as e:
+            print("PARSE ERROR:", e)
+            result = { 'images': [] }
         
         return ImageResponse(
             created=int(time.time()),
@@ -22,3 +43,10 @@ class KoboldCpp(CustomLLM):
         )
 
 kobold_cpp = KoboldCpp()
+
+# without this, optional_params doesnt work see https://github.com/BerriAI/litellm/blob/main/litellm/utils.py#L2171
+litellm.openai_compatible_providers.append('kobold')
+
+# we want to use pass_through_endpoints on / so we need to yank any default handlers
+app = litellm.proxy.proxy_server.app
+app.router.routes = [x for x in app.router.routes if x.path != '/']
