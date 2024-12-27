7b5e591304e87a1ec67ba1a1649771f62d1aee85
Mike <kryptk@gmail.com>
2024-12-27 18:36:07 +0000

    testing sdserver proxy

--- kobold_handler.py
+++ kobold_handler.py
@@ -22,10 +22,9 @@ class KoboldCpp(CustomLLM):
             optional_params['width'], optional_params['height'] = optional_params.pop('size').split('x')        
         if 'steps' not in optional_params:
             optional_params['steps'] = 8            
-        optional_params['prompt'] = prompt
-        
+        optional_params['prompt'] = prompt        
 
-        print("aimage_generation() called:", optional_params, "timeout=", timeout)
+        print("koboldcpp aimage_generation() called:", optional_params, "timeout=", timeout)
         try:
             response = await client.post("/sdapi/v1/txt2img", json=optional_params)
         except Exception as e:
@@ -42,10 +41,48 @@ class KoboldCpp(CustomLLM):
             data=[ImageObject(b64_json=img_data) for img_data in result["images"]]
         )
 
+class SDServer(CustomLLM):
+    async def aimage_generation(self, model: str, prompt: str, model_response: ImageResponse, optional_params: dict, logging_obj: Any, timeout: Optional[Union[float, httpx.Timeout]] = None, client = None, **kwargs) -> ImageResponse:
+        if client is None:
+            client = httpx.AsyncClient(base_url=kwargs['api_base'], timeout=timeout)
+        
+        # Health check. We don't actually need to generate anything, just check the server is alive.
+        if prompt == "test from litellm":
+            response = await client.get("/")
+            assert response.status_code == 404
+            return ImageResponse()
+
+        if 'n' in optional_params: optional_params['batch_count'] = optional_params.pop('n')
+        if 'size' in optional_params: optional_params['width'], optional_params['height'] = optional_params.pop('size').split('x')
+
+        if 'steps' not in optional_params: optional_params['steps'] = 8            
+        if 'negative_prompt' not in optional_params: optional_params['negative_prompt'] = "Bad quality, ugly"
+        if 'seed' not in optional_params: optional_params['seed'] = -1
+        optional_params['prompt'] = prompt
+        
+        print("sd-server aimage_generation() called:", optional_params, "timeout=", timeout)
+        try:
+            response = await client.post("/txt2img", json=optional_params)
+        except Exception as e:
+            print("HTTP ERROR:", e)
+            
+        try:
+            result = response.json()
+        except Exception as e:
+            print("PARSE ERROR:", e)
+            result = { 'images': [] }
+        
+        return ImageResponse(
+            created=int(time.time()),
+            data=[ImageObject(b64_json=img_data["data"]) for img_data in result]
+        )
+
 kobold_cpp = KoboldCpp()
+sd_server = SDServer()
 
 # without this, optional_params doesnt work see https://github.com/BerriAI/litellm/blob/main/litellm/utils.py#L2171
 litellm.openai_compatible_providers.append('kobold')
+litellm.openai_compatible_providers.append('sdserver')
 
 # we want to use pass_through_endpoints on / so we need to yank any default handlers
 app = litellm.proxy.proxy_server.app
